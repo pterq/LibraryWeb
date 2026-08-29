22828810f931542714caa43038eb0f322815c242
ptr <piotr2785.owca@gmail.com>
2026-08-29 23:41:02 +0200

    backend start

---
+++ Backend/library-web-backend/src/main/java/com/example/librarywebbackend/LibraryWebBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.librarywebbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryWebBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryWebBackendApplication.class, args);
+    }
+
+}
---
+++ Backend/library-web-backend/src/test/java/com/example/librarywebbackend/LibraryWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.librarywebbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryWebBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
