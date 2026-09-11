79394edbe55fc9e62b9f925a54b8ce6f846a834c
ptr <piotr2785.owca@gmail.com>
2026-09-11 17:30:43 +0200

    f

--- Backend/library-web-backend/src/main/java/com/example/librarywebbackend/service/implementation/FeeService.java
+++ Backend/library-web-backend/src/main/java/com/example/librarywebbackend/service/implementation/FeeService.java
@@ -43,7 +43,7 @@ public class FeeService implements IFeeService {
 
     @Override
     public List<FeeResponseDTO> getAllFees() {
-        return feeRepository.findAllWithGraph()
+        return feeRepository.findAll()
                 .stream()
                 .map(feeMapper::toResponse)
                 .toList();
